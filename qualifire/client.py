0afbd018894411466e43083b99fa7dbddfdae1a8
Dror Ivry <drorivry@gmail.com>
2024-10-31 15:32:34 +0200

    New python API

--- qualifire/client.py
+++ qualifire/client.py
@@ -1,12 +1,13 @@
-from typing import Union
+from typing import List, Union
 
 import json
 import logging
+from dataclasses import asdict
 
 import requests
 
 from .instrumentations import instrumentors
-from .types import EvaluationResponse, Input, Output
+from .types import EvaluationRequest, EvaluationResponse
 
 logger = logging.getLogger("qualifire")
 
@@ -15,12 +16,14 @@ class Client:
     def __init__(
         self,
         api_key: str,
-        base_url: str = "https://gateway.qualifire.ai",
+        base_url: str = "https://proxy.qualifire.ai",
         version: str = None,
+        debug: bool = False,
     ) -> None:
         self._base_url = base_url
         self._api_key = api_key
         self._version = version
+        self._debug = debug
 
     def initialize(self):
         for instrumentor_class in instrumentors:
@@ -45,26 +48,113 @@ class Client:
 
     def evaluate(
         self,
-        input: Union[str, Input],
-        output: Union[str, Output],
-        block: bool = True,
+        input: str,
+        output: str,
+        assertions: List[str] = None,
+        consistency_check: bool = False,
+        dangerous_content_check: bool = False,
+        hallucinations_check: bool = False,
+        harassment_check: bool = False,
+        hate_speech_check: bool = False,
+        pii_check: bool = False,
+        prompt_injections: bool = False,
+        sexual_content_check: bool = False,
     ) -> Union[EvaluationResponse, None]:
+        """
+        Evaluates the given input and output pairs.
 
-        url = f"{self._base_url}/api/evaluation/v1"
-        body = json.dumps({"async": not block, "input": input, "output": output})
+        :param assertions: A list of assertions to check.
+        :param consistency_check: Whether to check for consistency.
+        :param dangerous_content_check: Whether to check for dangerous content.
+        :param hallucinations_check: Whether to check for hallucinations.
+        :param harassment_check: Whether to check for harassment.
+        :param hate_speech_check: Whether to check for hate speech.
+        :param input: The input to evaluate.
+        :param messages: The messages to evaluate.
+        :param output: The output to evaluate.
+        :param pii_check: Whether to check for personally identifiable information.
+        :param prompt_injections: Whether to check for prompt injections.
+        :param responseFunctionCalls: The response function calls to check.
+
+        :return: An EvaluationResponse object.
+        :raises Exception: If an error occurs during the evaluation.
+
+        Example:
+
+        ```python
+        from qualifire import Qualifire
+
+        qualifire = Qualifire(api_key="your_api_key")
+
+        evaluation_response = qualifire.evaluate(
+            assertions=["The output should be a list of integers."],
+            consistency_check=True,
+            dangerous_content_check=True,
+            hallucinations_check=True,
+            harassment_check=True,
+            hate_speech_check=True,
+            input="Write a list of integers.",
+            output="[1, 2, 3]",
+            pii_check=True,
+            prompt_injections=True,
+            responseFunctionCalls="Write a list of integers.",
+            sexual_content_check=True,
+        )
+        """
+
+        url = f"{self._base_url}/api/evaluation/evaluate"
+        request = EvaluationRequest(
+            assertions=assertions,
+            consistency_check=consistency_check,
+            dangerous_content_check=dangerous_content_check,
+            hallucinations_check=hallucinations_check,
+            harassment_check=harassment_check,
+            hate_speech_check=hate_speech_check,
+            input=input,
+            output=output,
+            pii_check=pii_check,
+            prompt_injections=prompt_injections,
+            sexual_content_check=sexual_content_check,
+        )
+
+        body = json.dumps(asdict(request))
         headers = {
             "Content-Type": "application/json",
-            "X-qualifire-key": self._api_key,
+            "X-Qualifire-API-Key": self._api_key,
         }
 
-        if not block:
-            requests.post(url, headers=headers, data=body)
-            return None
-        else:
-            response = requests.post(url, headers=headers, data=body)
+        response = requests.post(url, headers=headers, data=body)
 
-            if response.status_code != 200:
-                raise Exception(f"Qualifire API error: {response.text}")
+        if response.status_code != 200:
+            raise Exception(f"Qualifire API error: {response.text}")
 
-            jsonResponse = response.json()
-            return EvaluationResponse(**jsonResponse)
+        jsonResponse = response.json()
+        return EvaluationResponse(**jsonResponse)
+
+    def invoke_evaluation(
+        self,
+        input: str,
+        output: str,
+        evaluation_id: str,
+    ):
+
+        url = f"{self._base_url}/api/evaluation/invoke/"
+
+        payload = {
+            "evaluation_id": evaluation_id,
+            "input": input,
+            "output": output,
+        }
+        headers = {
+            "X-Qualifire-API-Key": self._api_key,
+            "Content-Type": "application/json",
+        }
+
+        response = requests.request("POST", url, json=payload, headers=headers)
+        if response.status_code != 200:
+            if self._debug:
+                response.raise_for_status()
+            raise Exception(f"Qualifire API error: {response.text}")
+
+        jsonResponse = response.json()
+        return EvaluationResponse(**jsonResponse)
--- qualifire/types.py
+++ qualifire/types.py
@@ -1,87 +1,49 @@
-from typing import Dict, List, Optional
+from typing import List
 
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 
 
 @dataclass
-class Message:
+class LLMMessage:
+    content: str
     role: str
-    content: Optional[str] = None
 
 
 @dataclass
-class Choice:
-    index: int
-    message: Message
-    finish_reason: Optional[str] = None
-
-
-@dataclass
-class Usage:
-    prompt_tokens: int
-    completion_tokens: int
-    total_tokens: int
-
-
-@dataclass
-class Input:
-    model: str
-    messages: List[Message] = field(default_factory=list)
-    caller: Optional[str] = None
-
-
-@dataclass
-class Output:
-    id: str
-    model: str
-    choices: List[Choice] = field(default_factory=list)
-    created: Optional[int] = None
-    usage: Optional[Usage] = None
-    system_fingerprint: Optional[str] = None
-
-
-@dataclass
-class Evaluation:
-    async_: bool
-    input: Input
-    output: Output
-
-
-@dataclass
-class Result:
-    claim: str
-    contradiction: bool
-    passed: bool
-    matchScore: float
-    reason: str
-    quote: str
-    includedInContent: bool
-    monitorId: str
-    createdAt: str
-    organizationId: str
-    callId: str
+class EvaluationRequest:
+    input: str
+    output: str
+    consistency_check: bool
+    dangerous_content_check: bool
+    hallucinations_check: bool
+    harassment_check: bool
+    hate_speech_check: bool
+    pii_check: bool
+    prompt_injections: bool
+    sexual_content_check: bool
+    messages: List[LLMMessage] = None
+    assertions: List[str] = None
 
 
 @dataclass
 class EvaluationResult:
-    type: str
-    results: List[Result] = field(default_factory=list)
+    claim: str
+    confidence_score: int
+    label: str
+    name: str
+    quote: str
+    reason: str
+    score: int
 
 
 @dataclass
-class ScoreBreakdownItem:
-    length: int
-    scoreSum: float
+class EvaluationResultItem:
+    results: List[EvaluationResult]
+    type: str
 
 
 @dataclass
 class EvaluationResponse:
-    success: bool
-    score: float
+    evaluationResults: List[EvaluationResultItem]
+    score: int
     status: str
-    evaluationResults: List[EvaluationResult] = field(default_factory=list)
-    scoreBreakdown: Dict[str, ScoreBreakdownItem] = field(default_factory=dict)
-    defaultResponse: Optional[str] = None
-    block: Optional[bool] = None
-    retry: Optional[bool] = None
-    retryCap: Optional[int] = None
