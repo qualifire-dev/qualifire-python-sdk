eda67262d0afed5447c53890d3ff6808bc93ff83
Yuval <yuval@qualifire.ai>
2025-05-12 12:18:38 +0300

    Fix tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,5 @@
 from typing import Optional
 
-import os
-
 import pytest
 
 from qualifire.consts import (
@@ -23,11 +21,12 @@ from qualifire.utils import get_api_key, get_base_url
 def test_get_api_key(
     env_value: Optional[str],
     expected_error: bool,
+    monkeypatch: pytest.MonkeyPatch,
 ) -> None:
     if env_value is not None:
-        os.environ[QUALIFIRE_API_KEY_ENV_VAR] = env_value
+        monkeypatch.setenv(QUALIFIRE_API_KEY_ENV_VAR, env_value)
     else:
-        os.environ.pop(QUALIFIRE_API_KEY_ENV_VAR, None)
+        monkeypatch.delenv(QUALIFIRE_API_KEY_ENV_VAR, None)
 
     if expected_error:
         with pytest.raises(ValueError):
@@ -47,10 +46,11 @@ def test_get_api_key(
 def test_get_base_url(
     env_value: Optional[str],
     expected_result: str,
+    monkeypatch: pytest.MonkeyPatch,
 ) -> None:
     if env_value is not None:
-        os.environ[QUALIFIRE_BASE_URL_ENV_VAR] = env_value
+        monkeypatch.setenv(QUALIFIRE_BASE_URL_ENV_VAR, env_value)
     else:
-        os.environ.pop(QUALIFIRE_BASE_URL_ENV_VAR, None)
+        monkeypatch.delenv(QUALIFIRE_BASE_URL_ENV_VAR, None)
 
     assert get_base_url() == expected_result
